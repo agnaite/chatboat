b35bc800dd5a1f4c8ec5ae859c4b354438d311b4
agnaite <agnaite@me.com>
2018-02-05 16:03:29 -0800

    initial commit: - renders home page using revel - kafka consumer streams messages - kafka producer writes and enqueues

---
+++ app/controllers/app.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"github.com/agnaite/chatboat/app/consumer"
+	"github.com/agnaite/chatboat/app/producer"
+	"github.com/revel/revel"
+)
+
+func init() {
+	go producer.Producer()
+	go consumer.Consumer()
+}
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
